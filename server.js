c1f91828feb97336f1f39d6d50878550d4b14e2d
Bladimir Orellana <me@bladimirorellana.com>
2023-05-10 01:48:57 -0500

    upload

---
+++ server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+const express = require("express");
+var path = require("path");
+
+/////////////////////////
+// Uploads an image file
+/////////////////////////
+
+// Log the configuration
+
+const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+// Define middleware here
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname + "client/public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+
+// Connect to the Mongo DB
+
+mongoose.connect(
+  process.env.REACT_APP_MONGO_ATLAS_DB || "mongodb://127.0.0.1/hm-fencing"
+);
+
+app.listen(PORT, () => {
+  console.log("app on porst  " + PORT);
+});
